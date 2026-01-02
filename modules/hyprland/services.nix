a8df4954e7dba374023b7316c98df8aefda1d3ac
Tiago Scolari <git@tscolari.me>
2026-01-02 21:24:06 +0000

    Attempt to fix print prompt/portals

--- modules/hyprland/services.nix
+++ modules/hyprland/services.nix
@@ -149,7 +149,38 @@ in
         xdg-desktop-portal-gtk
         xdg-desktop-portal-hyprland
       ];
-      config.common.default = "*";
+      config = {
+        common = {
+          default = [ "gtk" ];
+        };
+        hyprland = {
+          default = [
+            "gtk"
+            "hyprland"
+          ];
+          "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+          "org.freedesktop.impl.portal.Print" = [ "gtk" ];
+        };
+      };
+    };
+
+    # On Ubuntu, we configure portals but don't install them via Nix
+    xdg.configFile."xdg-desktop-portal/portals.conf" = mkIf (!isNixOS) {
+      text = ''
+        [preferred]
+        default=gtk
+        org.freedesktop.impl.portal.FileChooser=gtk
+        org.freedesktop.impl.portal.Print=gtk
+      '';
+    };
+
+    xdg.configFile."xdg-desktop-portal/hyprland-portals.conf" = mkIf (!isNixOS) {
+      text = ''
+        [preferred]
+        default=gtk;hyprland
+        org.freedesktop.impl.portal.FileChooser=gtk
+        org.freedesktop.impl.portal.Print=gtk
+      '';
     };
 
     home.activation.xdgDesktopWarning = mkIf (!isNixOS) (
