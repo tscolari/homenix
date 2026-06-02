245f72cd94419aa118df84b0ba19492844d8ba80
Tiago Scolari <git@tscolari.me>
2026-06-02 15:03:53 +0100

    Add choose support for themes

--- modules/themes/_darwin.nix
+++ modules/themes/_darwin.nix
@@ -15,9 +15,15 @@ in
 
 {
   config = mkIf (cfg.enable && config.programs.homenix.enable && pkgs.stdenv.isDarwin) {
-    home.file.".config/homenix/bin/homenix-themes-choose" = {
-      source = ../../bin/darwin-homenix-themes-choose;
-      executable = true;
+    home.file = {
+      ".config/homenix/bin/homenix-choose" = {
+        source = ../../bin/darwin-choose;
+        executable = true;
+      };
+      ".config/homenix/bin/homenix-themes-choose" = {
+        source = ../../bin/darwin-homenix-themes-choose;
+        executable = true;
+      };
     };
   };
 }
