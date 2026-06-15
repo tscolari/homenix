dc3305eaf485e08307fef6c4e2a26cf4c0a061c0
Tiago Scolari <tiago.scolari@redpanda.com>
2026-06-15 09:56:33 +0100

    Make firefox_profiles work in MacOS

--- modules/firefox_profiles/default.nix
+++ modules/firefox_profiles/default.nix
@@ -9,7 +9,10 @@ with lib;
 
 let
 
-  nixGLWrapIfReq = pkg: if config.lib ? nixGL then config.lib.nixGL.wrap pkg else pkg;
+  nixGLWrapIfReq = pkg:
+    if pkgs.stdenv.isLinux && config.lib ? nixGL
+    then config.lib.nixGL.wrap pkg
+    else pkg;
 
   cfg = config.programs.homenix.firefox_profiles;
 
@@ -38,64 +41,109 @@ in
         (nixGLWrapIfReq cfg.package)
       ];
 
-      file = {
-        ".local/share/applications/work-firefox.desktop".text = ''
-          [Desktop Entry]
-          Actions=new-private-window;new-window;profile-manager-window
-          Categories=Network;WebBrowser
-          Exec=${firefoxBin} -P "work" --no-remote %U
-          GenericName=Web Browser
-          Icon=firefox
-          MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
-          Name=Work Firefox
-          StartupNotify=true
-          StartupWMClass=Work Firefox
-          Terminal=false
-          Type=Application
-          Version=1.4
-          X-Desktop-File-Install-Version=0.27
+      file = lib.mkMerge [
+        (lib.mkIf pkgs.stdenv.isLinux {
+          ".local/share/applications/work-firefox.desktop".text = ''
+            [Desktop Entry]
+            Actions=new-private-window;new-window;profile-manager-window
+            Categories=Network;WebBrowser
+            Exec=${firefoxBin} -P "work" --no-remote %U
+            GenericName=Web Browser
+            Icon=firefox
+            MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
+            Name=Work Firefox
+            StartupNotify=true
+            StartupWMClass=Work Firefox
+            Terminal=false
+            Type=Application
+            Version=1.4
+            X-Desktop-File-Install-Version=0.27
 
-          [Desktop Action new-private-window]
-          Exec=${firefoxBin} -P "work" --no-remote --private-window %U
-          Name=New Private Window
+            [Desktop Action new-private-window]
+            Exec=${firefoxBin} -P "work" --no-remote --private-window %U
+            Name=New Private Window
 
-          [Desktop Action new-window]
-          Exec=${firefoxBin} -P "work" --no-remote --new-window %U
-          Name=New Window
+            [Desktop Action new-window]
+            Exec=${firefoxBin} -P "work" --no-remote --new-window %U
+            Name=New Window
 
-          [Desktop Action profile-manager-window]
-          Exec=${firefoxBin} -P "work" --no-remote --ProfileManager
-          Name=Profile Manager
-        '';
-        ".local/share/applications/personal-firefox.desktop".text = ''
-          [Desktop Entry]
-          Actions=new-private-window;new-window;profile-manager-window
-          Categories=Network;WebBrowser
-          Exec=${firefoxBin} -P "personal" --no-remote %U
-          GenericName=Web Browser
-          Icon=firefox-nightly
-          MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
-          Name=Personal Firefox
-          StartupNotify=true
-          StartupWMClass=PersonalFirefox
-          Terminal=false
-          Type=Application
-          Version=1.4
-          X-Desktop-File-Install-Version=0.27
+            [Desktop Action profile-manager-window]
+            Exec=${firefoxBin} -P "work" --no-remote --ProfileManager
+            Name=Profile Manager
+          '';
+          ".local/share/applications/personal-firefox.desktop".text = ''
+            [Desktop Entry]
+            Actions=new-private-window;new-window;profile-manager-window
+            Categories=Network;WebBrowser
+            Exec=${firefoxBin} -P "personal" --no-remote %U
+            GenericName=Web Browser
+            Icon=firefox-nightly
+            MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
+            Name=Personal Firefox
+            StartupNotify=true
+            StartupWMClass=PersonalFirefox
+            Terminal=false
+            Type=Application
+            Version=1.4
+            X-Desktop-File-Install-Version=0.27
 
-          [Desktop Action new-private-window]
-          Exec=${firefoxBin} -P "personal" --no-remote --private-window %U
-          Name=New Private Window
+            [Desktop Action new-private-window]
+            Exec=${firefoxBin} -P "personal" --no-remote --private-window %U
+            Name=New Private Window
 
-          [Desktop Action new-window]
-          Exec=${firefoxBin} -P "personal" --no-remote --new-window %U
-          Name=New Window
+            [Desktop Action new-window]
+            Exec=${firefoxBin} -P "personal" --no-remote --new-window %U
+            Name=New Window
 
-          [Desktop Action profile-manager-window]
-          Exec=${firefoxBin} -P "Personal" --no-remote --ProfileManager
-          Name=Profile Manager
-        '';
-      };
+            [Desktop Action profile-manager-window]
+            Exec=${firefoxBin} -P "Personal" --no-remote --ProfileManager
+            Name=Profile Manager
+          '';
+        })
+
+        (lib.mkIf pkgs.stdenv.isDarwin {
+          "Applications/Work Firefox.app/Contents/Info.plist".text = ''
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+            <dict>
+              <key>CFBundleExecutable</key><string>launcher</string>
+              <key>CFBundleIdentifier</key><string>com.homenix.work-firefox</string>
+              <key>CFBundleName</key><string>Work Firefox</string>
+              <key>CFBundlePackageType</key><string>APPL</string>
+              <key>CFBundleVersion</key><string>1.0</string>
+            </dict>
+            </plist>
+          '';
+          "Applications/Work Firefox.app/Contents/MacOS/launcher" = {
+            executable = true;
+            text = ''
+              #!/bin/bash
+              exec "${firefoxBin}" -P "work" --no-remote "$@"
+            '';
+          };
+          "Applications/Personal Firefox.app/Contents/Info.plist".text = ''
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+            <dict>
+              <key>CFBundleExecutable</key><string>launcher</string>
+              <key>CFBundleIdentifier</key><string>com.homenix.personal-firefox</string>
+              <key>CFBundleName</key><string>Personal Firefox</string>
+              <key>CFBundlePackageType</key><string>APPL</string>
+              <key>CFBundleVersion</key><string>1.0</string>
+            </dict>
+            </plist>
+          '';
+          "Applications/Personal Firefox.app/Contents/MacOS/launcher" = {
+            executable = true;
+            text = ''
+              #!/bin/bash
+              exec "${firefoxBin}" -P "personal" --no-remote "$@"
+            '';
+          };
+        })
+      ];
     };
   };
 }
