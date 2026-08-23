5dc1c69cdff8751fa0f91d656c42f5661d82adc6
Tiago Scolari <git@tscolari.me>
2026-08-23 15:52:02 +0100

    Remove invalid swayosd-server option

--- modules/hyprland/swayosd.nix
+++ modules/hyprland/swayosd.nix
@@ -18,11 +18,6 @@ in
       # topMargin = 0.9;
     };
 
-    systemd.user.services.swayosd.Service.ExecStart = mkForce [
-      "" # Clear existing ExecStart
-      "${config.services.swayosd.package}/bin/swayosd-server --hide-caps-lock-led"
-    ];
-
     home.file = {
       ".config/swayosd/config.toml".source = ../../configs/swayosd/config.toml;
       ".config/swayosd/style.css".source = ../../configs/swayosd/style.css;
